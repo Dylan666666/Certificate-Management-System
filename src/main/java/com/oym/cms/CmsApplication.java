564cdb530e9ed1dc4f060160228fb3c79805bff4
Dylan <1439620614@qq.com>
2022-02-23 16:33:27 +0800

    first commit

---
+++ src/main/java/com/oym/cms/CmsApplication.java
@@ -0,0 +1,16 @@
+package com.oym.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 14396
+ */
+@SpringBootApplication
+public class CmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsApplication.class, args);
+    }
+
+}
